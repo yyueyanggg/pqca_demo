808b01f87328d03e7394aa3c95e94ea02d2835d7
yyueyangg <cheryueyang@gmail.com>
2025-07-14 23:33:10 +0800

    FIX: fix yml file

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-"from cryptography.hazmat.primitives import hashes" 
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+import os
+
+# This would be detected by CBOMkit
+key = Fernet.generate_key()
+f = Fernet(key)
+token = f.encrypt(b"my secret message")
